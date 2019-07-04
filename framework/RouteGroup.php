0c10391916e0b57768092b8cb077fad94f17bf25
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-07-04 18:16:58 +0200

    added route group

---
+++ framework/RouteGroup.php
@@ -0,0 +1,19 @@
+<?php
+  namespace Framework;
+
+  class RouteGroup {
+    protected $_route;
+    protected $_group_builder;
+
+    function __construct(string $route, \Closure $group_builder) {
+      $this->_route = $route;
+      $this->_group_builder = $group_builder;
+    }
+
+    function get_router() {
+      $router = new Router($this->_route);
+      $builder = $this->_group_builder;
+      $builder($router);
+      return $router;
+    }
+  }
