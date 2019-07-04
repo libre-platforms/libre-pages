f09a113ade978621b479c980d3e76175b696a542
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-07-04 21:31:46 +0200

    request now takes params

--- framework/Request.php
+++ framework/Request.php
@@ -9,6 +9,7 @@
     function __construct(array& $request_data) {
       $this->_method = $request_data['method'];
       $this->_path = $request_data['path'];
+      $this->_params = $request_data['params'];
     }
 
     function method() {
@@ -23,10 +24,11 @@
       return $this->_params;
     }
 
-    static function from_current_request() {
+    static function from_current_request(array& $params) {
       $request_data = [
         'method' => $_SERVER['REQUEST_METHOD'],
         'path' => strlen($_SERVER['REQUEST_URI']) > 1 ? \rtrim($_SERVER['REQUEST_URI'], '/') : $_SERVER['REQUEST_URI'],
+        'params' => &$params,
       ];
 
       return new Request($request_data);
