848b5efb3abf9e5d58d62352317d08d0a01c9bee
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-07-04 22:17:24 +0200

    added proper status code handling; start toString method

--- framework/Response.php
+++ framework/Response.php
@@ -2,5 +2,18 @@
   namespace Framework;
 
   class Response {
-    protected $status_code = StatusCode::HTTP_OK;
+    protected $_status_code = StatusCode::HTTP_OK;
+
+    function& status_code(?int $status_code = null) {
+      if ($status_code) {
+        $this->_status_code = $status_code;
+        return $this;
+      } else {
+        return $this->_status_code;
+      }
+    }
+
+    function __toString() {
+      return (string)$this->status_code();
+    }
   }
