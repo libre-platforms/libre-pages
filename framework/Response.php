b19ae2a786ac7d505b3ec26dc9b8b27eefdc84ea
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-07-05 08:57:10 +0200

    renamed status code method; added content type; added json response method

--- framework/Response.php
+++ framework/Response.php
@@ -3,10 +3,11 @@
 
   class Response {
     protected $_status_code = StatusCode::HTTP_OK;
+    protected $_content_type = 'text/html';
 
     protected $_content = '';
 
-    function& status_code(?int $status_code = null) {
+    function& status(?int $status_code = null) {
       if ($status_code) {
         $this->_status_code = $status_code;
         return $this;
@@ -15,13 +16,27 @@
       }
     }
 
+    function& content_type(?string $content_type = null) {
+      if ($content_type) {
+        $this->_content_type = $content_type;
+        return $this;
+      } else {
+        return $this->_content_type;
+      }
+    }
+
     function& write(string $content) {
       $this->_content .= $content;
       return $this;
     }
 
+    function& json($json) {
+      return $this->content_type('application/json')->write(json_encode($json));
+    }
+
     function __toString() {
       \http_response_code($this->_status_code);
+      header('Content-Type:'.$this->_content_type);
       return $this->_content;
     }
   }
