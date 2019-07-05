124de14fc03939156edb27d0383e6c0f5eaa6476
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-07-05 10:16:41 +0200

    added function to create view evaluators; fixed naming convention

--- framework/functions.php
+++ framework/functions.php
@@ -1,9 +1,16 @@
 <?php
   namespace Framework;
 
-  function makeAssetLoader(Request& $request) {
+  function make_asset_loader(Request& $request) {
     $asset_base = "http".($request->https ? 's' : '').'://'.$request->server_name.':'.$request->server_port.'/assets/';
     return function(string $stored_asset_path) use (&$asset_base) {
       return $asset_base.$stored_asset_path;
+    };
+  }
+
+  function make_view_evaluator(string $view_base_path) {
+    return function(array $view, array $data = []) use (&$view_base_path) {
+      extract($data);
+      require $view_base_path.implode(DIRECTORY_SEPARATOR, $view).'.php';
     };
   }
