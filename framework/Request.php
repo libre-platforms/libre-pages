7f03ab8fbc053fb5266501a0126484ca39ec3d95
Jörn Neumeyer <contact@joern-neumeyer.de>
2019-07-06 12:34:50 +0200

    added notices for license and copyright

--- framework/Request.php
+++ framework/Request.php
@@ -1,4 +1,21 @@
 <?php
+//  Copyright (C) 2019 Jörn Neumeyer
+//
+//  This file is part of LibrePages.
+//
+//  LibrePages is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  LibrePages is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with LibrePages.  If not, see <https://www.gnu.org/licenses/>.
+
   declare(strict_types=1);
   namespace Framework;
 
