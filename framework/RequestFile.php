84357e9921a2094632f1ef82ad233c482a4c15c8
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-07-06 10:50:59 +0200

    added docs and reworked save method

--- framework/RequestFile.php
+++ framework/RequestFile.php
@@ -4,6 +4,8 @@
   namespace Framework;
 
   /**
+   * Represents a file, which has been uploaded with a request.
+   *
    * @property string $name
    * @property string $type
    * @property string $tmp_name
@@ -12,6 +14,7 @@
    */
   class RequestFile {
     protected $_data;
+    protected $_is_saved = false;
 
     function __construct(array $data) {
       $this->_data = $data;
@@ -21,10 +24,19 @@
       return $this->_data ?? null;
     }
 
+    /**
+     * Trys to save an uploaded file to the given location.
+     */
     function save(string $destination) {
-      return move_uploaded_file($this->tmp_name, $destination);
+      if (!$this->_is_saved) {
+        $this->_is_saved = move_uploaded_file($this->tmp_name, $destination);
+      }
+      return $this->_is_saved;
     }
 
+    /**
+     * Returns a bool, representing the success of the file upload.
+     */
     function is_ok() {
       return $this->error === UPLOAD_ERR_OK;
     }
