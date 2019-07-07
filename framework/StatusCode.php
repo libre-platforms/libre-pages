8178abf4cea43a1c3adeaa2f0daddf904c4188ba
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-07-07 09:22:58 +0200

    status message is set automatically; custom message is unnecessary

--- framework/StatusCode.php
+++ framework/StatusCode.php
@@ -21,12 +21,4 @@
 
   class StatusCode {
     const HTTP_OK = 200;
-
-    private static $status_messages = [
-      200 => 'OK',
-    ];
-
-    static function message_of_code(int $code) {
-      return self::$status_messages[$code] ?? '';
-    }
   }
