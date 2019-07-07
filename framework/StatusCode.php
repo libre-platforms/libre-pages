65461b03599ca659ff882aa9c8400385d864a9ba
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-07-07 10:08:59 +0200

    added more status codes

--- framework/StatusCode.php
+++ framework/StatusCode.php
@@ -20,5 +20,30 @@
   namespace Framework;
 
   class StatusCode {
-    const HTTP_OK = 200;
+    const CONTINUE = 100;
+    const SWITCHING_PROTOCOLS = 101;
+    const PROCESSING = 102;
+    const EARLY_HINTS = 103;
+
+    const OK = 200;
+    const CREATED = 201;
+    const ACCEPTED = 202;
+    const NON_AUTHORITATIVE_INFORMATION = 203;
+    const NO_CONTENT = 204;
+    const RESET_CONTENT = 205;
+
+    const MULTIPLE_CHOICES = 300;
+    const MOVED_PERMANENTLY = 301;
+    const FOUND = 302;
+    const SEE_OTHER = 303;
+    const USE_PROXY = 305;
+    const SWITCH_PROXY = 306;
+    const TEMPORARY_REDIRECT = 307;
+
+    const BAD_REQUEST = 400;
+    const UNAUTHORIZED = 401;
+    const PAYMENT_REQUIRED = 402;
+    const FORBIDDEN = 403;
+    const NOT_FOUND = 404;
+    const METHOD_NOT_ALLOWED = 405;
   }
