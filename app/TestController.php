f4529e18fbb8171c547a14e3d2aa29a17e3c7c12
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-07-10 20:56:43 +0200

    added type annotations

--- app/TestController.php
+++ app/TestController.php
@@ -1,8 +1,10 @@
 <?php
   namespace App;
 
+  use Framework\{Request, Response};
+
   class TestController {
-    static function user_by_id(&$request, &$response) {
+    static function user_by_id(Request &$request, Response &$response) {
       $response->write('hello from controller');
       return $response;
     }
