174b7cefa2df7c1dc18bd4ac8950d9e947377f49
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-07-25 15:55:15 +0200

    added more sample test cases

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -4,11 +4,25 @@
   use Framework\TestCase;
 
   class ExampleTest extends TestCase {
-    function test_example() {
+    function test_success() {
       $this->assert(true, 'This test assertion should succeed!');
     }
 
     function test_fail() {
-      $this->assert(false, 'This test assertion should fail!');
+      $this->assert(false, 'This test assertion fails!');
+    }
+
+    function test_invalid_exception() {
+      $this->expect_exception(\Exception::class);
+      throw new \ErrorException();
+    }
+
+    function test_exception() {
+      $this->expect_exception(\ErrorException::class);
+      throw new \ErrorException();
+    }
+
+    function test_missing_exception() {
+      $this->expect_exception(\ErrorException::class);
     }
   }
