1acbbb3263f1388a9b89ce218a22fe09f113e922
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-07-29 21:42:33 +0200

    added test script to watch

--- test-watch.sh
+++ test-watch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 clear
 php test.php $*
-while inotifywait -e modify tests/*.php app/*.php framework/*.php
+while inotifywait -e modify tests/*.php app/*.php framework/*.php test.php
 do
   clear
   php test.php $*
