fb6bb70a86f1a5294ecfe2c44f10e48bbaa45864
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-08-01 01:05:13 +0200

    fixed test watch

--- test-watch.sh
+++ test-watch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 clear
 php test.php $*
-while inotifywait -e modify tests/*.php app/*.php framework/*.php test.php
+while inotifywait -e modify tests/*.php app/*.php pages/*.php test.php
 do
   clear
   clear
