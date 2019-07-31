52703e33919f5e240091d4360e9eea761f1f53e6
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-07-31 23:23:29 +0200

    updated routes

--- routes.php
+++ routes.php
@@ -16,47 +16,4 @@
 //  You should have received a copy of the GNU Affero General Public License
 //  along with LibrePages.  If not, see <https://www.gnu.org/licenses/>.
 
-  use Framework\{Request, Response, Router, Validation};
-
-  $router->get('/', function(Request $request, Response $response) {
-    return $response->write('moin');
-  });
-
-  $router->get('/foo', function($request, $response) {
-    return $response->write('bar');
-  });
-
-  $router->get('/foo/{bla}', function($request, $response) {
-    return $response->write($request->params['bla']);
-  });
-
-  $router->group('/grp', function (Router $router) {
-    $router->get('/bla', function(Request $request, Response $response) {
-      return $response->write('bla');
-    });
-
-    $router->get('/u-{abc:\d+}', Router::make_handler_chain([
-      function(Request $request, Response $response, callable $next) {
-        $response->status(404);
-        return $next($request, $response);
-      },
-      function(Request $request, Response $response) {
-        $loader = Framework\make_asset_loader($request);
-        // $response->json(['hello_image' => $loader('img/hello-world.png')]);
-        $response->view(['welcome']);
-        $response->set_cookie('foo', 'bar');
-        return $response;
-      }
-    ]));
-
-    $router->post('/validated-route', Router::make_handler_chain([
-      Validation::body('foo')->is_numeric(),
-      'Framework\\Validation::reject_on_error',
-      function(Request $request, Response $response) {
-        $response->write($request->body['foo']);
-        return $response;
-      }
-    ]));
-
-    $router->post('/u-{abc:\d+}', 'App\\TestController::user_by_id');
-  });
+  $router->get('/', 'App\\ViewController::index');
