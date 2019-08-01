3a6a16285ab117569e6eac8f330aea3b2dcdd3c4
Jörn Neumeyer <joernneumeyer@protonmail.ch>
2019-08-01 12:22:18 +0200

    updated phan config

--- .phan/config.php
+++ .phan/config.php
@@ -1,6 +1,6 @@
 <?php
   return [
     'target_php_version' => '7.2',
-    'directory_list' => ['app/', 'framework/'],
+    'directory_list' => ['app/', 'pages/'],
     'exclude_analysis_directory_list' => ['vendor/']
   ];
