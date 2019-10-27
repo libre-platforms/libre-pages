2c70631d9db0cbc94ee2a2f2b25944c1b424a2a3
Jörn Neumeyer <contact@joern-neumeyer.de>
2019-10-27 21:29:12 +0100

    replaced old closure with lambda

--- pages/functions.php
+++ pages/functions.php
@@ -23,9 +23,7 @@
    */
   function make_asset_loader(Request& $request) {
     $asset_base = "http".($request->https ? 's' : '').'://'.$request->server_name.':'.$request->server_port.'/assets/';
-    return function(string $stored_asset_path) use (&$asset_base) {
-      return $asset_base.$stored_asset_path;
-    };
+    return fn(string $stored_asset_path) => $asset_base.$stored_asset_path;
   }
 
   /**
