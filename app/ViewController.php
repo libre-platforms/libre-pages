ba809f396254f90326fbbdf234c2e8df6f20149f
Jörn Neumeyer <contact@joern-neumeyer.de>
2019-10-28 17:14:20 +0100

    added class doc

--- app/ViewController.php
+++ app/ViewController.php
@@ -3,6 +3,9 @@
 
   use Pages\{Request, Response};
 
+  /**
+   * Controller for simple views.
+   */
   class ViewController {
     static function index(Request &$request, Response &$response) {
       return $response->view(['index'], ['request' => $request]);
